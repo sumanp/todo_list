745731304cfb59050dee719cf6f549cd11092e09
Suman Puri <sumanpuri55@gmail.com>
2014-06-26 14:13:31 +0530

    initial commit

---
+++ web.rb
@@ -0,0 +1,65 @@
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+require 'json'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
+  class Item
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :done, Boolean, :required => true, :default => false
+  property :created, DateTime
+  end
+DataMapper.finalize.auto_upgrade!
+
+
+
+get '/' do
+  @item=Item.all(:order=> :created.desc)
+  redirect '/new' if @item.empty? 
+  erb :index	
+end
+
+
+get '/new' do
+	@title="Add todo item"
+	erb :new
+end
+
+post '/new' do
+	Item.create(:content=> params[:content], :created=>Time.now)
+	redirect '/'
+end
+
+
+post '/done' do
+  item = Item.first(:id => params[:id])
+  item.done = !item.done
+  item.save
+  content_type 'application/json'
+  value = item.done ? 'done' : 'not done'
+  { :id => params[:id], :status => value }.to_json
+end
+
+get '/delete/:id' do
+	@item=Item.first(:id => params[:id])
+	erb :delete
+end
+
+delete '/delete/:id' do
+	if params.has_key?("ok")
+		item = Item.first(:id => params[:id])
+		item.destroy
+		redirect '/'
+	else
+		redirect '/'
+	end
+end
+
+
+
+
+
+
+
